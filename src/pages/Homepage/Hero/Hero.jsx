827bdf883e852c64318640ade478d1eecee26015
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-04 00:04:01 +0600

    social media links

--- src/pages/Homepage/Hero/Hero.jsx
+++ src/pages/Homepage/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import image2 from "../../../assets/profile-pic (2).png";
 import resume from "../../../assets/Basic_Resume.docx.pdf";
 import back from "../../../assets/back.jpg";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -19,18 +20,35 @@ const Hero = () => {
       <div className="relative z-10 text-center md:text-start">
         <h3 className="text-2xl font-semibold">Hello, I'm</h3>
         <h1 className="text-5xl font-bold mb-2">Ansarul Islam</h1>
-        <h1 className="text-3xl font-semibold text-gray-300">Frontend Developer</h1>
+        <h1 className="text-3xl font-semibold text-gray-300">
+          Frontend Developer
+        </h1>
         <p className="max-w-lg mt-4 text-lg">
           Turning lines of code into captivating digital experiences. Let's
           build something amazing!
         </p>
+        <h3 className="uppercase mt-6 mb-4">Find me on</h3>
+        <div className="flex text-4xl gap-4 mb-6">
+          <a href="https://www.facebook.com/ansarulislamriyad" target="_blank">
+            <FaFacebook className="cursor-pointer hover:scale-125 transition-all duration-200" />
+          </a>
+          <a href="https://github.com/AnsarulIslam10" target="_blank">
+            <FaGithub className="cursor-pointer hover:scale-125 transition-all duration-200" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ansarul-islam-32a229318/"
+            target="_blank"
+          >
+            <FaLinkedin className="cursor-pointer hover:scale-125 transition-all duration-200" />
+          </a>
+        </div>
         <a href={resume} download>
           <button className="btn btn-outline mt-6 hover:bg-white hover:text-black transition duration-300">
             Download Resume
           </button>
         </a>
       </div>
-      
+
       <div className="relative z-10 mt-6 md:mt-0">
         <img
           className="w-[300px] rounded-full shadow-lg transform transition duration-300 hover:scale-105"

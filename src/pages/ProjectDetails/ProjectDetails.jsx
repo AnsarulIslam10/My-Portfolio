72871e634297f3238a76c7340f7baab96a1673ee
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-05 11:22:27 +0600

    fix

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -27,7 +27,7 @@ const ProjectDetails = () => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-slate-800 text-white">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 text-white">
       <div className="text-center">
         <div className="flex justify-between items-center mb-4">
           <Link

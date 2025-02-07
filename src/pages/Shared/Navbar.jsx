7c1a159aa06f97aa6de4e8d86f460f75712f3c0c
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-02-07 14:38:41 +0600

    navbar

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,3 +1,4 @@
+import { FaBars } from "react-icons/fa";
 import resume from "../../assets/Resume_of_Ansarul_Islam.pdf";
 import { Link } from 'react-scroll';
 const Navbar = () => {
@@ -52,37 +53,24 @@ const Navbar = () => {
     <div className="navbar sticky top-0 z-20 backdrop-blur-lg">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+          <div tabIndex={0} role="button" className="btn btn-ghost px-0 mr-2 text-2xl lg:hidden text-cyan-500">
+            <FaBars/>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 z-[1] mt-3 w-32 p-2 shadow text-lg space-y-2"
           >
             {links}
           </ul>
         </div>
-        <a className=" text-cyan-500 font-bold text-lg md:text-3xl">Ansarul Islam</a>
+        <a className=" text-cyan-500 font-bold text-lg md:text-3xl uppercase">Ansarul Islam</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
         <a href={resume} download>
-          <button className="btn animate__animated animate__pulse animate__infinite	infinite animate__slower	3s  shiny-button text-gray-900 font-bold uppercase rounded-md hover:bg-cyan-600 border-none bg-cyan-500">Download Resume</button>
+          <button className="btn btn-sm sm:btn-md animate__animated animate__pulse animate__infinite	infinite animate__slower	3s  shiny-button text-gray-900 font-bold uppercase rounded-md hover:bg-cyan-600 border-none bg-cyan-500">Download Resume</button>
         </a>
       </div>
     </div>

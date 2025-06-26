718b11728fc18a1441ecf31619fe57da5df88f82
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-06-26 14:11:38 +0600

    education update

--- src/pages/Homepage/Education/Education.jsx
+++ src/pages/Homepage/Education/Education.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import educationAnimation from "../../../assets/education.json";
 import cnpi from "../../../assets/cnpi.jpeg";
+import nub from "../../../assets/NUB.png"
 import Lottie from "lottie-react";
 
 const Education = () => {
@@ -11,9 +12,9 @@ const Education = () => {
       </h2>
       {/* Container with overlay */}
       <div
-        className="flex flex-col md:flex-row items-center justify-around bg-slate-800 p-8 rounded-lg shadow-md relative overflow-hidden"
+        className="flex flex-col md:flex-row items-center justify-around bg-slate-800 p-8 py-16 rounded-lg shadow-md relative overflow-hidden"
         style={{
-          backgroundImage: `url(${cnpi})`,
+          backgroundImage: `url(${nub})`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
           backgroundSize: "cover",
@@ -23,22 +24,22 @@ const Education = () => {
         <div className="absolute inset-0 bg-black opacity-80 rounded-lg"></div>
 
         {/* Education Details */}
-        <div className="relative z-10 w-full  mb-8 md:mb-0">
-          <h3 className="text-4xl font-semibold mb-6 text-cyan-500">
-            Diploma in Engineering
-          </h3>
-          <p className="text-xl text-cyan-200 mb-2">
-            Institution: Chapainabganj Polytechnic Institute
+        <div className="relative z-10 w-full  mb-8 md:mb-0 text-center">
+          {/* <h3 className="text-4xl font-semibold mb-6 text-cyan-500">
+            Bachelor of Science
+          </h3> */}
+          <p className="text-3xl text-cyan-300 font-bold mb-2">
+            Northern University Bangladesh
           </p>
-          <p className="text-xl text-cyan-200 mb-2">
-            Department: Computer Technology
+          <p className="text-2xl text-cyan-300 font-bold mb-2">
+            BSc in Computer Science & Engineering
           </p>
-          <p className="text-lg text-cyan-200 mb-2">Status: Complete</p>
-          <p className="text-lg text-cyan-200 mb-4">Duration: 4 Years</p>
+          <p className="text-xl text-cyan-200 mb-4">Semester: 1st</p>
+          <p className="text-xl text-cyan-200 mb-2">Status: Ongoing</p>
         </div>
 
         {/* Relevant Coursework */}
-        <div className="relative z-10 w-full md:w-1/2">
+        {/* <div className="relative z-10 w-full md:w-1/2">
           <h4 className="text-2xl font-semibold text-cyan-500 mb-4">
             Relevant Coursework:
           </h4>
@@ -56,7 +57,7 @@ const Education = () => {
               Networking
             </li>
           </ul>
-        </div>
+        </div> */}
 
       </div>
     </section>
